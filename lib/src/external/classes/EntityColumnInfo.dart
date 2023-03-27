5212e95c619d4f6776f58b41b9b38240a76860e3
non.auth0 <truecollaboration0@gmail.com>
2023-03-27 18:33:26 +0200

    0.1.2: stable

--- lib/src/external/classes/EntityColumnInfo.dart
+++ lib/src/external/classes/EntityColumnInfo.dart
@@ -10,7 +10,7 @@ class EntityColumnInfo<T> extends ColumnInfo {
       super.defaultValue,
       super.isPrimaryKey = false,
       super.isAutoIncrement = false,
-      super.isNullable = true,
+      super.isNullable = false,
       super.extra,
   }) : super(
     name: name,
